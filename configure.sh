6767a427c9da56ba2e5fb51f226915152f7b8ce9
dreamskr <43511466+dreamskr@users.noreply.github.com>
2025-11-09 13:02:22 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -15,4 +15,4 @@ echo "src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git;main"
 echo "src-git istore https://github.com/linkease/istore;main" >> feeds.conf.default
 
 # add feed
-echo "src-git nikki https://github.com/nikkinikki-org/OpenWrt-nikki.git;main" >> "feeds.conf.default"
+echo "src-git nikki https://github.com/nikkinikki-org/OpenWrt-nikki.git;main" >> feeds.conf.default
